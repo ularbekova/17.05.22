8da0c072180735132375a42b39f0a7d066ce04f0
Ularbekova <milanaularbekova1@gmail.com>
2022-05-17 19:41:19 +0600

    MILANA

---
+++ script.js
@@ -0,0 +1,28 @@
+// // let button = document.getElementById("btn")
+// // button.addEventListener("click",func)
+
+// // function func(){
+// //     alert('you clicked me')
+// // }
+// //mousemove
+//mouseout
+
+// let butt = document.querySelector('.plus')
+// butt.addEventListener('click',fun)
+// let h = document.getElementById('p')
+
+// function fun(){
+//    p.style.display ='none'
+   
+// }
+
+let butt = document.querySelector('.plus')
+butt.addEventListener('click',fun)
+let h = document.getElementById('p')
+
+function fun(){
+   p.style.display ='none'
+   
+}
+
+
